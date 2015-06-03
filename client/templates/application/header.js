05a077eb63e34f9e4933827c98ff0f74c455f60e
bbhamilton <bartekbilicki@gmail.com>
2015-06-04 00:21:44 +0200

    single promise basic template
    based on id

--- client/templates/application/header.js
+++ client/templates/application/header.js
@@ -9,39 +9,6 @@ Template.header.helpers({
     return _.where(promisesData, {progress: 'notfullfilled'}).length;
   },
   inProgressPromisses: function() {
-    console.log('----');
-    console.dir(_.where(promisesData, {progess: 'inprogress'}));
-    console.log('----');
     return _.where(promisesData, {progress: 'inprogress'}).length;
   }
-});
-
-Template.header.events({
-  'click .all': function(e) {
-    e.preventDefault();
-    $('nav li').removeClass('active');
-    $('nav .all').addClass('active');
-    $('#promises .promise').show();
-  },
-  'click .fullfilled': function(e) {
-    e.preventDefault();
-    $('nav li').removeClass('active');
-    $('nav .fullfilled').addClass('active');
-    $('#promises .promise').hide();
-    $('#promises .fullfilled').show();
-  },
-  'click .notfullfilled': function(e) {
-    e.preventDefault();
-    $('nav li').removeClass('active');
-    $('nav .notfullfilled').addClass('active');
-    $('#promises .promise').hide();
-    $('#promises .notfullfilled').show();
-  },
-  'click .inprogress': function(e) {
-    e.preventDefault();
-    $('nav li').removeClass('active');
-    $('nav .inprogress').addClass('active');
-    $('#promises .promise').hide();
-    $('#promises .inprogress').show();
-  },
 });
--- client/templates/promises/promisesList.js
+++ client/templates/promises/promisesList.js
@@ -1,3 +1,33 @@
 Template.promisesList.helpers({
   promises: promisesData
 });
+
+Template.promisesList.events({
+  'click nav .all': function(e) {
+    e.preventDefault();
+    $('nav li').removeClass('active');
+    $('nav .all').addClass('active');
+    $('#promises .promise').show();
+  },
+  'click nav .fullfilled': function(e) {
+    e.preventDefault();
+    $('nav li').removeClass('active');
+    $('nav .fullfilled').addClass('active');
+    $('#promises .promise').hide();
+    $('#promises .fullfilled').show();
+  },
+  'click nav .notfullfilled': function(e) {
+    e.preventDefault();
+    $('nav li').removeClass('active');
+    $('nav .notfullfilled').addClass('active');
+    $('#promises .promise').hide();
+    $('#promises .notfullfilled').show();
+  },
+  'click nav .inprogress': function(e) {
+    e.preventDefault();
+    $('nav li').removeClass('active');
+    $('nav .inprogress').addClass('active');
+    $('#promises .promise').hide();
+    $('#promises .inprogress').show();
+  },
+});
